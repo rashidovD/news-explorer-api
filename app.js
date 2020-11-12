4964fca616241bda59939f46bb62ce2c17f733db
Dzhavid Rashidov <mr.telepuzig@yandex.ru>
2020-11-12 16:53:33 +0300

    fixfix

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     'http://localhost:3000',
+    'http://rashfront.students.nomoreparties.co',
     'http://localhost:3001',
     'http://localhost:3005',
     'https://rashidovD.github.io/news-explorer-frontend',
